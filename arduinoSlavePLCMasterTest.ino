645664991619427346ac609587dbd5b3707cbb7b
s94285 <s94285@yahoo.com.tw>
2017-05-31 14:02:57 +0800

    final commit

--- arduinoSlavePLCMasterTest.ino
+++ arduinoSlavePLCMasterTest.ino
@@ -1,3 +1,7 @@
+#include <Wire.h>
+#include <dht.h>
+#include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
 #include <modbusSlave.h>
 #include <modbusRegBank.h>
 #include <modbusDevice.h>
@@ -5,24 +9,35 @@
 
 modbusDevice regBank;
 modbusSlave slave;
+dht DHT;
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 bool input[8];
 
 #define RS485RxTxEnablePin 2
 #define RS485Baud 9600
-#define RS485Format SERIAL_8E1
+#define RS485Format SERIAL_8E1 //define for Serial 8 even 1
 
 void setup()
 {
-	for (int pin = 22; pin <= 29; pin++) {
-		pinMode(pin, INPUT);
+	lcd.begin(16, 2);
+	lcd.clear();
+
+	for (byte i = 0; i<3; i++) {  //flash LCD backlight 3 times for boot indicating
+		lcd.noBacklight();
+		delay(250);
+		lcd.backlight();
+		delay(250);
 	}
 
 	for (int pinOut = 31; pinOut <= 44; pinOut++) {
 		pinMode(pinOut, OUTPUT);
-		digitalWrite(pinOut, LOW);
+		digitalWrite(pinOut, HIGH);
 	}
-	regBank.setId(1);
-	regBank.add(40001);
+
+	regBank.setId(1); //Set Modbus Slave ID
+	regBank.add(40001); //Add hoding register 40001 = temperature
+	regBank.add(40002); //40002 = target Temp
+	regBank.add(40003); //40003 = LED state
 
 	slave._device = &regBank;
 
@@ -30,27 +45,51 @@ void setup()
 
 }
 
-long unsigned boolArrayToLong(bool boolArray[8]) {
-	long unsigned value = 0;
-	for (int j = 0; j < 8; j++) {
-		value += boolArray[j] << j;
-	}
-	return value;
-}
 
 void loop()
 {	
-	int i = 0;
-	int pin = 22;
-	while(i<8){
-			
-			input[i] = digitalRead(pin);
-			Serial.print(i);
-			Serial.println(input[i]);
-			
-			i++;
-			pin++;
+	for (int i = 0; i <=10; i++) { //LCD refresh every 500ms
+		DHT.read11(A0);
+		regBank.set(40001, (word)DHT.temperature); //put temperature in register
+		slave.run();
+		toggleLED();
+		delay(50);
 	}
-	regBank.set(40001, boolArrayToLong(input));
-	slave.run();
+	readTempToLCD();
+}
+
+
+void readTempToLCD(){  //show informations on LCD
+	lcd.setCursor(0, 0);
+	lcd.print("Now = ");
+	lcd.print((float)DHT.humidity, 0);
+	lcd.print(" %  ");
+	lcd.print((float)DHT.temperature, 0);
+	lcd.print(" C");
+	lcd.setCursor(0, 1);
+	lcd.print("Target = ");
+	lcd.print(regBank.get(40002));
+	lcd.print(" C");
+}
+
+void toggleLED() {  //Change LED state by reading register
+	word toggleWord = regBank.get(40003);
+	for (int j = 0; j <= 8; j++) {
+		input[j] = toggleWord % 2;
+		toggleWord /= 2;
+	}
+	digitalWrite(31, (input[0]) ? LOW : HIGH);
+	digitalWrite(32, (input[1]) ? LOW : HIGH);
+	digitalWrite(33, (input[2]) ? LOW : HIGH);
+	digitalWrite(34, (input[2]) ? LOW : HIGH);
+	digitalWrite(35, (input[2]) ? LOW : HIGH);
+	digitalWrite(36, (input[3]) ? LOW : HIGH);
+	digitalWrite(37, (input[3]) ? LOW : HIGH);
+	digitalWrite(38, (input[3]) ? LOW : HIGH);
+	digitalWrite(39, (input[3]) ? LOW : HIGH);
+	digitalWrite(40, (input[4]) ? LOW : HIGH);
+	digitalWrite(41, (input[5]) ? LOW : HIGH);
+	digitalWrite(42, (input[6]) ? LOW : HIGH);
+	digitalWrite(43, (input[7]) ? LOW : HIGH);
+	digitalWrite(44, (input[7]) ? LOW : HIGH);
 }
